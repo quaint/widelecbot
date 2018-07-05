0d2d865718b5c0d8a7d26d1586c0167aafc1291e
Tomasz Wiśniewski <twisnia@gmail.com>
2018-07-05 15:05:23 +0200

    reminder added

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var builder = require('botbuilder');
 var request = require('request');
 var cheerio = require('cheerio');
 
-var url = 'http://nawidelcukoszalin.pl';
-
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -23,6 +21,7 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
      session.send('Zamawiamy do 11:30 przez formularz -> https://goo.gl/forms/1L9wzEJwxzCD8Hnm1');
+     session.send('Pamiętamy o przyniesieniu karteczki!');
      session.send('Weryfikacja -> https://docs.google.com/spreadsheets/d/1qf2s7RV5QeVXcSNXK1KI12oiMWMGyb9bWFBUjVhVQLo');
      session.endDialog();
 });
