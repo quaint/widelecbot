97ed4536f38290be9a244951835b2f4a2d485caf
Tomasz Wiśniewski <twisnia@gmail.com>
2018-09-03 09:03:13 +0200

    end lines removed

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ bot.dialog('menu', (session, args, next) => {
     request(url, function(err, resp, body) {
         $ = cheerio.load(body);
         menuTag = $('#menu > div > div > div.vc_col-sm-4.wpb_column.vc_column_container > div > div > div.wpb_text_column > div');
-        session.send($(menuTag).text());
+        session.send($(menuTag).text().replace(/\n\n/g, ""));
         session.send('Zamawiamy do 11:30 przez formularz -> https://goo.gl/forms/1W45f7VSnWU9HW6o2');
         session.endDialog();
     });
